13192225c2c6c6d1b71ad0903004970e6002bae6
nicosrm <86198952+nicosrm@users.noreply.github.com>
2025-03-12 14:52:43 +0100

    Add move validation section.

---
+++ assets/code/ECPKnight::isMoveValid.cpp
@@ -0,0 +1,18 @@
+bool ECPKnight::isMoveValid(ECPChessField newField) {
+    // ...
+    const int columnDiff =
+        abs((int) newField.column -(int) currentField.column);
+    const int rowDiff =
+        abs((int) newField.row - (int) currentField.row);
+
+    if (columnDiff == 2 && rowDiff == 1) {
+        // two field horizontally and one vertically
+        return true;
+    }
+    if (columnDiff == 1 && rowDiff == 2) {
+        // two fields vertically and one horizontally
+        return true;
+    }
+    
+    return false;
+};
